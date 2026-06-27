e1b9b446f4f3ad096ed1bec94419e8dd07bc7246
Sajid Hussain <sajidhuseyin@gmailcom>
2026-06-27 15:51:48 +0500

    Fixed ForecastDay type

--- src/types/weather.ts
+++ src/types/weather.ts
@@ -1,83 +1,66 @@
  export type TempUnit = "C" | "F";
 
-export interface WeatherData {
+export interface ForecastDay {
+  date:string;
 
-location:{
-name:string;
-country:string;
-};
+  day:{
+    maxtemp_c:number;
+    maxtemp_f:number;
 
-current:{
+    mintemp_c:number;
+    mintemp_f:number;
 
-temp_c:number;
-temp_f:number;
+    condition:{
+      text:string;
+      icon:string;
+    };
+  };
 
-humidity:number;
+  astro:{
+    sunrise:string;
+    sunset:string;
+  };
 
-wind_kph:number;
+  hour:{
+    time:string;
 
-feelslike_c:number;
-feelslike_f:number;
+    temp_c:number;
+    temp_f:number;
 
-condition:{
-text:string;
-icon:string;
-};
+    condition:{
+      text:string;
+      icon:string;
+    };
 
-};
-
-forecast:{
-
-forecastday:[
-
-{
-
-date:string;
-
-day:{
-
-maxtemp_c:number;
-maxtemp_f:number;
-
-mintemp_c:number;
-mintemp_f:number;
-
-condition:{
-
-text:string;
-icon:string;
-
-};
-
-};
-
-astro:{
-
-sunrise:string;
-sunset:string;
-
-};
-
-hour:{
-
-time:string;
-
-temp_c:number;
-temp_f:number;
-
-condition:{
-
-text:string;
-icon:string;
-
-};
-
-}[];
-
+  }[];
 }
 
-];
+export interface WeatherData {
 
-};
+  location:{
+    name:string;
+    country:string;
+  };
+
+  current:{
+    temp_c:number;
+    temp_f:number;
+
+    humidity:number;
+
+    wind_kph:number;
+
+    feelslike_c:number;
+    feelslike_f:number;
+
+    condition:{
+      text:string;
+      icon:string;
+    };
+  };
+
+  forecast:{
+    forecastday: ForecastDay[];
+  };
 
 }
